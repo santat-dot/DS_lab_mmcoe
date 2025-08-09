af1eefa212d53c8af41d505090f91beae0d1f5b3
Sanskar Tathe <sanskartathe2024.comp@mmcoe.edu.in>
2025-08-09 18:55:54 +0530

    Updated activity_3.cpp

--- activity_3.cpp
+++ activity_3.cpp
@@ -4,38 +4,37 @@ For example, imagine you have a set of servers that handle requests for a web ap
 #include <iostream>
 using namespace std;
 
-int main() {
-    int indexes[100], count = 0; 
-    char q;
+void hashing(){
 
+int indexes[100], count = 0; 
+    char q;
     while (1) {
         int server, ID, server_index;
         cout << "Enter the total number of servers: ";
         cin >> server;
         cout << "Enter your ID: ";
         cin >> ID;
-
-        server_index = ID % server;
+          server_index = ID % server;
         indexes[count++] = server_index;
-
         cout << "Your request is accepted by Server: " << server_index << endl;
-
         cout << "Do you want to continue? "<<endl;
         cout << "Enter Y if yes "<<endl;
         cout << "Enter 'n' or 'N' if no "<<endl;
-        
-        cin >> q;
-        
-        cout<<endl<<endl;
+         cin >> q;
+         cout<<endl<<endl;
         if (q == 'n' || q == 'N') {
 			break;
 			
 		}
     }
-
     cout << "\nserver indexes is: ";
     for (int i = 0; i < count; i++) {
         cout << indexes[i]<<" ";
     }
+}
+int main() {
+	
+	hashing();
+    
 }
 
