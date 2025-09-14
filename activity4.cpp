9f19abd6942058cabf6727a88424cb5d16044dab
Sanskar Tathe <sanskartathe2024.comp@mmcoe.edu.in>
2025-09-15 01:54:07 +0530

    activity4.cpp

--- activity4.cpp
+++ activity4.cpp
@@ -3,74 +3,70 @@ using namespace std;
 
 class Node {
 public:
-    string Task_Name;
+    string task_name;
     int priority;
     int exe_time;
-    Node* next;
+    Node *next;
 
-    
-    Node(string task, int p, int t) {
-        Task_Name = task;
+      Node(string t, int p, int e) {
+        task_name = t;
         priority = p;
-        exe_time = t;
-        next = nullptr;
+        exe_time = e;
+        next = NULL;
     }
 
     void display() {
-        cout << "task Name: " << Task_Name << ", ";
-        cout << "priority: " << priority << ", ";
-        cout << "pxecution Time: " << exe_time << endl;
+        cout << "Task Name: " << task_name
+             << ", Priority: " << priority
+             << ", Execution Time: " << exe_time<< endl;
     }
 };
 
 
-Node* insert(Node* head, string task, int priority, int exe_time) {
-    Node* newNode = new Node(task, priority, exe_time);
-
-    
-    if (head == nullptr || priority > head->priority) {
-        newNode->next = head;
-        return newNode;
-    }
-
-    Node* temp = head;
-   
-    while (temp->next != nullptr && temp->next->priority >= priority) {
-        temp = temp->next;
-    }
-
-    newNode->next = temp->next;
-    temp->next = newNode;
-
-    return head;
-}
-
 int main() {
-    Node* head = nullptr;
-    int m;
-    string task;
-    int priority, exe_time;
+    int n;
+    Node *header = NULL, *temp = NULL, *t, *prev;
+    string tn;
+    int p, e;
+    cout << "enter No. of Tasks: ";
+    cin >> n;
 
-    cout << "How many nodes do you want to ";
-    cin >> m;
+    for(int i = 0; i < n; i++) {
+        cout << "enter Task Name : ";
+        cin >> tn;
+        
+        cout << "enter Priority : ";
+        cin >> p;
+        cout << "enter Execution Time : ";
+        cin >> e;
 
-    for (int i = 0; i < m; i++) {
-        cout << "Task Name: ";
-        cin >> task;
-        cout << "Enter Priority  ";
-        cin >> priority;
-        cout << " Execution Time: ";
-        cin >> exe_time;
+        temp = new Node(tn, p, e);
 
-        head = insert(head, task, priority, exe_time);
+        if(header == NULL || p > header->priority) {
+           
+           temp->next = header;
+           header = temp;
+
+        }
+        else {
+           
+            t = header;
+            while(t != NULL && t->priority >= p) {
+                prev = t;
+                t = t->next;
+            }
+            prev->next = temp;
+            temp->next = t;
+        }
     }
 
-    cout << "\n List of Tasks :\n";
-    Node* temp = head;
-    while (temp != nullptr) {
-        temp->display();
-        temp = temp->next;
+   
+    t = header;
+    while(t != NULL) {
+        t->display();
+        t = t->next;
     }
 
     return 0;
 }
+
