59190e62bef129cc5518b30b1b8302be332686d7
Sanskar Tathe <sanskartathe2024.comp@mmcoe.edu.in>
2025-10-30 18:50:47 +0530

    activity-5.cpp

--- activity-5.cpp
+++ activity-5.cpp
@@ -1,103 +1,110 @@
 #include <iostream>
+#include <cstring>
+#include <string>
+#define N 10
 using namespace std;
-
-class Stack {
-public:
-    int top;
-    string arr; 
-
-    Stack() {
-        top = -1;
-        arr = "";
-    }
-
-    int isFull() {
-        
-        return 0;
-    }
-
-    int isEmpty() {
-        if (top == -1)
-            return 1;
-        else
-            return 0;
-    }
-
-    void push(char x) {
-        top = top + 1;      
-        arr.push_back(x);  
-    }
-
-    void pop() {
-        if (isEmpty()) {
-            cout << "Stack underflow" << endl;
-        } else {
-            arr.pop_back();  
-            top = top - 1;
-        }
-    }
-
-    char peek() {
-        if (isEmpty())
-            return '\0';
-        else
-            return arr[top];
-    }
+class stack
+{
+	public:
+ char arr[10];
+ string ex;
+ int top;
+	stack()
+	{
+    	top=-1;
+	}
+	void push(char c)
+	{
+    	if(top==(N-1))
+    	{cout<<"stack overflow";}
+    	else{top+=1;
+    	arr[top]=c;}
+	}
+	char pop()
+	{   char c= ' ';
+    	if(top==-1)
+    	{//cout<<"Stack underflow";
+    	}
+    	else
+    	{
+        	c=arr[top];
+        	top --;
+    	}
+    	return c;
+	}
+	int precedence(char opr){
+    	if (opr=='*' || opr=='/')
+        	return 2;
+    	if (opr=='+' || opr=='-')
+        	return 1;
+   	if(opr=='(')  return 0;
+	}
+	char associativity(char opr){
+    	if (opr=='*' || opr=='/'||opr=='+' || opr=='-')
+        	return 'L';
+    	else
+        	return 'R';
+	}
+	char peek(){return arr[top]; }
+	string InfixToPostfixConversion(string ex);
 };
 
+string stack::InfixToPostfixConversion(string ex){
+	int l=ex.length();
+	int i=0,j=0;
+	char op_exp[20];
+	char ch,ch1;
+	while(i<l){
 
-int check(char open, char close) {
-    if ((open == '(' && close == ')') ||
-        (open == '{' && close == '}') ||
-        (open == '[' && close == ']'))
-        return 1;
-    else
-        return 0;
-}
+     	//cout<<ex[i];  
+     	if(ex[i]=='+' ||ex[i]=='-'||ex[i]=='*' || ex[i]=='/'){
+       	 
+        	if(top==-1) push(ex[i]);
+        	else{
+             	ch=peek();
+             	//if(ch== ' ')
+             	while(precedence(ex[i])<=precedence(ch)){
+               	ch1=pop();
+               	op_exp[j++]=ch1;
+               	ch=peek();
+            	}
+            	push(ex[i]);
+        	}
+     	}
+    	else if(ex[i]=='(')
+        	push(ex[i]);
+    	else if(ex[i]==')'){
+        	ch1=pop();
+        	while(ch1!='('){
+            	op_exp[j++]=ch1;
+            	ch1=pop();
+        	}
+    	}
+    	else  {
+        	op_exp[j++]=ex[i];
+       	// var++;
+    	}
+    	i++;
+   	 
+	}
+	do{
+    	ch=pop();
+    	op_exp[j++]=ch;
+	}while(ch!=' ');
+	op_exp[j]='\0';
+	return op_exp;
+    
+	}
+ 
+ int main() {  
+  stack s;
+  string ex;
+  cout<<"\n Enter an expression:";
+  cin>>ex;
+  string op_exp=s.InfixToPostfixConversion(ex);
+  cout<<"\n Postfix Expression is: "<<op_exp;
+  return 0;
+	 
+   }
 
 
-int isBalanced(string expr) {
-    Stack s;
-
-    for (int i = 0; i < expr.length(); i++) {
-        char ch = expr[i];
-
-       
-        if (ch == '(' || ch == '{' || ch == '[') {
-            s.push(ch);
-        }
-        
-        else if (ch == ')' || ch == '}' || ch == ']') {
-            if (s.isEmpty()) {
-                return 0; 
-            } 
-            else if (check(s.peek(), ch) == 0) {
-                return 0; 
-            } 
-            else {
-                s.pop(); 
-            }
-        }
-    }
-
-   
-    if (s.isEmpty())
-        return 1;
-    else
-        return 0;
-}
-
-int main() {
-    string expr;
-
-    cout << "Enter an expression: ";
-    cin >> expr;
-
-    if (isBalanced(expr) == 1) {
-        cout << "Expression is well parenthesized." << endl;
-    } else {
-        cout << "Expression is NOT well parenthesized." << endl;
-    }
-
-    return 0;
-}
